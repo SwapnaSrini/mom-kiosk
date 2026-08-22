735a0837ae7f92a66f8463f37783c2631a09827a
SwapnaSrini <swapnasrinivasan8@gmail.com>
2026-08-22 17:55:40 +0200

    restructured Tabs, Added Nav for Product Details Page, added more properties to Product model (for images)

--- src/pages/Products/ProductsDetails.tsx
+++ src/pages/Products/ProductsDetails.tsx
@@ -1,10 +1,55 @@
+import { Link, useParams } from "react-router-dom"
+import { products } from "../../data/ProductData"
+import "./ProductsDetails.css"
+
 function ProductDetailsPage() {
-  return (
-    <div>
-      <h1>Product Details</h1>
-      <p>This is where the product details will appear.</p>
+  const { id } = useParams()
+
+  const productId = Number(id)
+
+const product = products.find(
+  (item) => Number(item.id) === productId
+)
+
+if (!product) {
+  return <h1>Product not found, try again later</h1>
+}
+ return (
+  <main className="product-details">
+
+    <Link to="/products" className="back-link">
+      ← Back to Products
+    </Link>
+
+    <div className="products-details-layout">
+
+      <div className="products-details-image">
+        <img
+          src={product.thumbnailUrl}
+          alt={product.title}
+        />
+      </div>
+
+      <div className="products-details-info">
+
+        <h1>{product.title}</h1>
+
+        <p>{product.condition}</p>
+
+        <p>📍 {product.location}</p>
+
+        <p>Given away by {product.sellerName}</p>
+
+        <button>
+          Contact Mom
+        </button>
+
+      </div>
+
     </div>
-  )
+
+  </main>
+)
 }
 
 export default ProductDetailsPage
